7af1ca1f7982568e0c46d0985623d40da42b683f
Kesarion <alexandru.sfirlogea@gmail.com>
2016-07-10 23:38:21 +0300

    updated sample

--- sample.js
+++ sample.js
@@ -6,18 +6,6 @@ var Manga = require('./');
 
 co(function *() {
     let manga = new Manga((yield Manga.findManga('Bleach'))[0].url, './Bleach');
-
     yield manga.getVolumes();
-    let loadingStarted = false;
-    let interval = setInterval(function () {
-        let loading = manga.loading['TBD'];
-        if (loading) {
-            loadingStarted = true;
-            console.log(`${loading.done} Done ${loading.buffered} Buffered ${loading.total} Total`);
-        } else if (loading) {
-            console.log('Finished!');
-            clearInterval(interval);
-        }
-    }, 3000);
-    yield manga.saveVolume('TBD');
+    yield manga.saveVolume(manga.volumes.length - 1);
 }).catch(console.log);
